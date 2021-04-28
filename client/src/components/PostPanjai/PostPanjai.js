9e89983c77847ee3d084f6228b83feadb870e87b
junjira11 <forth12345678@gmail.com>
2021-04-29 01:35:06 +0700

    forth

--- client/src/components/PostPanjai/PostPanjai.js
+++ client/src/components/PostPanjai/PostPanjai.js
@@ -15,86 +15,6 @@ import {
 } from "@material-ui/core";
 import PostPanjaiForm from "./PostPanjaiForm";
 import ButterToast, { Cinnamon } from "butter-toast";
-<<<<<<< HEAD
-import { DeleteSweep, AccessAlarm, ThreeDRotation, AssignmentTurnedIn } from "@material-ui/icons";
-import moment from 'moment';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { If, Then, ElseIf, Else } from 'react-if-elseif-else-render';
-import Icon from '@material-ui/core/Icon';
-import Axios from 'axios';
-// import Slideshow from './Slideshow';
-import SlideShow from 'react-image-show';
-
-import './PostPanjai.css'
-const currentUser = localStorage.getItem('currentUser')
-const currentUser_id = localStorage.getItem('currentUser_id')
-
-
-const styles = theme => ({
-    paper: {
-        margin: theme.spacing(3),
-        padding: theme.spacing(2),
-
-    },
-    smMargin: {
-        "&:hover": {
-            backgroundColor: "rgba(85, 52, 4, 0.925)"
-        },
-        margin: theme.spacing(1),
-        background: 'rgba(187, 130, 44, 0.925)'
-    },
-    smMargin1: {
-        "&:hover": {
-            backgroundColor: "rgba(85, 52, 4, 0.925)"
-        },
-        margin: theme.spacing(1),
-        background: '#a13800'
-    },
-    actionDiv: {
-        textAlign: "center"
-    },
-    // กรอบที่ใส่โพส
-    post1: {
-
-        borderRadius: '20px',
-        boxShadow: '0 1px 1px 1px rgba(85, 52, 4, 0.925)',
-        height: 'auto',
-        padding: '30px 30px',
-        marginBlock: '15px'
-
-    },
-    // กรอบโพส
-    framepost: {
-        // background: '#f9a825',
-        borderRadius: 5,
-        boxShadow: '1px   1px 1px 1px rgba(187, 130, 44, 0.925)',
-        color: 'rgba(187, 130, 44, 0.925)',
-        height: '100%',
-        padding: '10px 10px',
-        marginBlock: '15px',
-        borderRadius: '20px',
-    },
-    frampicture: {
-        padding: '10px 10px'
-
-    },
-    picture: {
-        height: '150px',
-        width: 'auto',
-        margin: '10px auto',
-
-
-    },
-    frontpost: {
-        fontFamily: 'mali',
-        borderRadius: '50px'
-    },
-    color1: {
-        color: '#a13800'
-=======
 import {
   DeleteSweep,
   AccessAlarm,
@@ -109,9 +29,9 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { If, Then, ElseIf, Else } from "react-if-elseif-else-render";
 import Icon from "@material-ui/core/Icon";
 import Axios from "axios";
-import Slideshow from "./Slideshow";
-import SlideShow from "react-image-show";
 
+import SlideShow from "react-image-show";
+import './PostPanjai.css'
 const currentUser = localStorage.getItem("currentUser");
 const currentUser_id = localStorage.getItem("currentUser_id");
 
@@ -123,7 +43,6 @@ const styles = (theme) => ({
   smMargin: {
     "&:hover": {
       backgroundColor: "rgba(85, 52, 4, 0.925)",
->>>>>>> 4b26c6614bc540cec916f49c600a60e4d53d32fa
     },
     margin: theme.spacing(1),
     background: "rgba(187, 130, 44, 0.925)",
@@ -376,18 +295,16 @@ const PostPanjai = ({ classes, ...props }) => {
                         }),
                         (
                           <Grid container justify="center">
-                            <SlideShow
+                            <SlideShow className="imageslide"
                               images={Array_image}
-                              width="200px"
-                              imagesWidth="200px"
-                              imagesHeight="100px"
+                              // width="0px"
+                              imagesWidth="400px"
+                              imagesHeight="200px"
                               imagesHeightMobile="56vw"
-                              thumbnailsWidth="920px"
+                              thumbnailsWidth="520px"
                               thumbnailsHeight="12vw"
                               className={classes.picture}
-                              indicators
-                              thumbnails
-                              fixedImagesHeight
+                              indicators thumbnails fixedImagesHeight
                             />
                           </Grid>
                         ))
@@ -478,149 +395,9 @@ const PostPanjai = ({ classes, ...props }) => {
   );
 };
 
-<<<<<<< HEAD
-                                                <Grid container>
-                                                    <Grid item xs={8}>
-                                                        <Typography variant='h5' className={`${classes.color1} ${classes.frontpost}`}>
-                                                            {record.title}
-                                                        </Typography>
-                                                    </Grid>
-
-                                                    {currentUser !== record.creator && <Grid item sm={4} className={classes.judjudjud}>
-                                                        <IconButton
-                                                            aria-label="more"
-                                                            aria-controls="long-menu"
-                                                            aria-haspopup="true"
-                                                            onClick={handleClick}
-
-                                                        >
-                                                            <MoreVertIcon />
-                                                        </IconButton>
-                                                        <Menu
-                                                            id="long-menu"
-                                                            anchorEl={anchorEl}
-                                                            keepMounted
-                                                            open={open}
-                                                            onClose={handleClose}
-                                                            PaperProps={{
-                                                                style: {
-                                                                    maxHeight: ITEM_HEIGHT * 4.5,
-                                                                    width: '20ch',
-                                                                },
-                                                            }}
-                                                        >
-                                                            {options.map((option) => (
-                                                                <MenuItem key={option} onClick={() => handleChange(record._id, option)}>
-                                                                    {option}
-                                                                </MenuItem>
-                                                            ))}
-                                                        </Menu>
-
-                                                    </Grid>}
-
-                                                </Grid>
-
-                                                <div className={classes.frontpost}>
-                                                    ข้อมูล : {record.message}
-                                                </div>
-
-                                                {
-                                                    Array_image = [],
-                                                    record.image.map((image, index) => {
-                                                        Array_image.push('http://localhost:3001/image/' + image)
-                                                    }),
-                                                    < Grid container justify="center">
-                                                        <SlideShow className="imageslide"
-                                                            images={Array_image}
-                                                            width="400px"
-                                                            imagesWidth="400px"
-                                                            imagesHeight="200px"
-                                                            imagesHeightMobile="56vw"
-                                                            thumbnailsWidth="520px"
-                                                            thumbnailsHeight="12vw"
-                                                            className={classes.picture}
-                                                            indicators thumbnails fixedImagesHeight
-                                                        />
-                                                    </Grid>
-                                                }
-
-                                                <div className={`${classes.color1} ${classes.frontpost}`}>
-                                                    โทร : {record.contect}
-                                                </div>
-                                                <div className={`${classes.color1} ${classes.frontpost}`}>
-                                                    จังหวัด : {record.location}
-                                                </div>
-                                                <div className={`${classes.color1} ${classes.frontpost}`}>
-                                                    ผู้สร้าง : {record.creator}
-                                                </div>
-                                                <div className={`${classes.color1} ${classes.frontpost}`}>
-                                                    {moment(record.Timestamp).calendar()}
-                                                </div>
-                                                <Grid container justify="center">
-                                                    <div className={classes.botton1}>
-                                                        <If condition={currentUser == record.creator}>
-                                                            <Then>
-                                                                <Button variant="contained" color="primary" size="small"
-                                                                    className={`${classes.smMargin} ${classes.frontpost}`}// จำเป็น
-                                                                    onClick={() => ScrollToTop(record._id)}>
-                                                                    แก้ไข
-                                                                </Button>
-                                                                <Button variant="contained" color="secondary" size="small"
-                                                                    className={`${classes.smMargin1} ${classes.frontpost}`}
-                                                                    onClick={() => onDelete(record._id)}>
-                                                                    ลบ
-                                                                </Button>
-                                                            </Then>
-
-                                                            <ElseIf condition={currentUser == 'admin'}>
-                                                                <Button variant="contained" color="secondary" size="small"
-                                                                    className={`${classes.smMargin1} ${classes.frontpost}`}
-                                                                    onClick={() => onDelete(record._id)}>
-                                                                    ลบ
-                                                                </Button>
-                                                            </ElseIf>
-
-                                                            <Else>
-                                                                <Button variant="contained" color="primary" size="small"
-                                                                    className={`${classes.smMargin} ${classes.frontpost}`}// จำเป็น
-                                                                    onClick={() => requestItem(record._id)}
-                                                                >
-                                                                    ขอรับ
-                                                                </Button>
-                                                                <Button variant="contained" color="secondary" size="small"
-                                                                    className={`${classes.smMargin1} ${classes.frontpost}`}
-                                                                    onClick={() => favoriteItem(record._id)}
-                                                                >
-                                                                    ถูกใจ
-                                                                </Button>
-                                                            </Else>
-                                                        </If>
-
-                                                    </div>
-                                                </Grid>
-
-                                                {/* รูปแบบช่อง */}
-                                            </ListItemText>
-                                        </ListItem>
-                                    </Fragment>
-                                </Paper>
-                            </Grid>
-                        )
-                    })
-                }
-            </Grid>
-        </>
-    );
-}
-
-const mapStateToProps = state => ({
-    postPanjaiList: state.postPanjai.list
-})
-=======
 const mapStateToProps = (state) => ({
   postPanjaiList: state.postPanjai.list,
 });
->>>>>>> 4b26c6614bc540cec916f49c600a60e4d53d32fa
 
 const mapActionToProps = {
   fetchAllPostPanjai: action.fetchAll,
